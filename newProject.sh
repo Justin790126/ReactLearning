f9e10fa97f229a1e0242086a3a9dd1e04a501bac
justin.chang <justin.chang@quantatw.com>
2017-04-10 14:46:00 +0800

    Environment create

---
+++ newProject.sh
@@ -0,0 +1,9 @@
+npm init
+
+npm install --save-dev babel-core babel-eslint babel-loader babel-preset-es2015 babel-preset-react html-webpack-plugin webpack webpack-dev-server
+
+npm install --save react react-dom
+
+npm install --save react react-dom react-router
+
+npm install --save-dev babel-core babel-eslint babel-loader babel-preset-es2015 babel-preset-react eslint eslint-config-airbnb eslint-loader eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-react webpack webpack-dev-server html-webpack-plugin
